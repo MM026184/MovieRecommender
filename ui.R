07f8cb089cf1b692ad27c71c81f470c2d603652f
McClanalytics <mcclanahanm12@gmail.com>
2021-12-05 21:36:59 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -13,29 +13,54 @@ shinyUI(
           skin = "blue",
           dashboardHeader(title = "Movie Recommender"),
           
-          dashboardSidebar(disable = TRUE),
-
-          dashboardBody(includeCSS("css/movies.css"),
-              fluidRow(
-                  box(width = 12, title = "Step 1: Rate as many movies as possible", status = "info", solidHeader = TRUE, collapsible = TRUE,
-                      div(class = "rateitems",
-                          uiOutput('ratings')
-                      )
-                  )
-                ),
-              fluidRow(
-                  useShinyjs(),
-                  box(
+          dashboardSidebar(
+            sidebarMenu(
+                # Setting id makes input$tabs give the tabName of currently-selected tab
+                id = "tabs",
+                menuItem("Popular Movies by Genre", tabName = "byGenre", icon = icon("hotjar")),
+                menuItem("Discover Movies for You", tabName = "byCollab", icon = icon("users"))
+            )
+          ),
+          dashboardBody(
+            includeCSS("css/movies.css")
+            ,tabItems(
+                tabItem("byCollab",
+                    fluidRow(
+                        box(width = 12, title = "Step 1: Rate as many movies as possible", status = "info", solidHeader = TRUE, collapsible = TRUE,
+                            div(class = "rateitems",
+                                uiOutput('ratings')
+                            )
+                        )
+                    )
+                )
+                ,tabItem("byGenre",
+                    fluidRow(
+                        box(width = 12, title = "Step 1: Select a movie genre", status = "info", solidHeader = TRUE, collapsible = TRUE,
+                            selectInput("selectedGenre","Genre",
+                                choices = c("Action", "Adventure", "Animation", 
+                                "Children's", "Comedy", "Crime",
+                                "Documentary", "Drama", "Fantasy",
+                                "Film-Noir", "Horror", "Musical", 
+                                "Mystery", "Romance", "Sci-Fi", 
+                                "Thriller", "War", "Western")
+                            )
+                        )
+                    )
+                )
+            )
+            ,fluidRow(
+                useShinyjs(),
+                box(
                     width = 12, status = "info", solidHeader = TRUE,
                     title = "Step 2: Discover movies you might like",
                     br(),
                     withBusyIndicatorUI(
-                      actionButton("btn", "Click here to get your recommendations", class = "btn-warning")
+                    actionButton("btn", "Click here to get your recommendations", class = "btn-warning")
                     ),
                     br(),
                     tableOutput("results")
-                  )
-               )
-          )
+                )
+            )
+        )
     )
 ) 
